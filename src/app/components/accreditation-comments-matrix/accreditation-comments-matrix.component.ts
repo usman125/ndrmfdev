8cdc1346a653dd4edecfb985b65f5b864242c852
Usman Ali Khan <khan@Usmans-MacBook-Pro.local>
2020-03-21 22:40:14 +0500

    fixing Fip Qualification styles

--- src/app/components/accreditation-comments-matrix/accreditation-comments-matrix.component.ts
+++ src/app/components/accreditation-comments-matrix/accreditation-comments-matrix.component.ts
@@ -28,7 +28,7 @@ import {
 
     trigger('pageAnimations', [
       transition(':enter', [
-        query('.hero, form', [
+        query('.heroes, form', [
           style({opacity: 0, transform: 'translateY(-100px)'}),
           stagger(-30, [
             animate('500ms cubic-bezier(0.35, 0, 0.25, 1)', style({ opacity: 1, transform: 'none' }))
--- src/app/components/fip-qualification/fip-qualification.component.ts
+++ src/app/components/fip-qualification/fip-qualification.component.ts
@@ -28,7 +28,7 @@ import { Router } from "@angular/router";
   animations: [
     trigger('pageAnimations', [
       transition(':enter', [
-        query('.hero, form', [
+        query('.btn-row, form', [
           style({ opacity: 0, transform: 'translateY(-100px)' }),
           stagger(-30, [
             animate('500ms cubic-bezier(0.35, 0, 0.25, 1)', style({ opacity: 1, transform: 'none' }))
