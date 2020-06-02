04e595da500ed4a681c58a78ec9c832fba955ede
Usman Ali Khan <usmanalikhan00@gmail.com>
2020-06-02 12:40:04 +0500

    Adding few changes to appraisal dashboard

--- src/app/components/dmpam-home/dmpam-home.component.ts
+++ src/app/components/dmpam-home/dmpam-home.component.ts
@@ -9,6 +9,9 @@ import { ProjectService } from 'src/app/services/project.service';
 })
 export class DmpamHomeComponent implements OnInit {
 
+  projectStats: any = null;
+  apiLoading: boolean = false;
+
   constructor(
     private _router: Router,
     private _projectService: ProjectService
@@ -19,11 +22,24 @@ export class DmpamHomeComponent implements OnInit {
   }
 
   getAllProject(){
+    this.apiLoading = true;
     this._projectService.getAllProjects().subscribe(
-      result => {
+      (result: any) => {
         console.log("DM PM ALL PROJECTS:--", result);
+        var preCount = 0;
+        var extCount = 0;
+        result.forEach(element => {
+          if (element.status === "Extended Appraisal") extCount = extCount + 1;
+          if (element.status === "Preliminary Appraisal") preCount = preCount + 1;
+        });
+        this.projectStats = {
+          preCount,
+          extCount
+        }
+        this.apiLoading = false;
       },
       error => {
+        this.apiLoading = false;
         console.log("ERROR DM PM ALL PROJECTS:--", error);
       }
     );
