c13d0d46353a4c1f1a7be540f6312ebc2787b6c7
Usman Ali Khan <usmanalikhan00@gmail.com>
2020-08-12 13:35:55 +0500

    changing the edit user

--- src/app/components/edit-user/edit-user.component.ts
+++ src/app/components/edit-user/edit-user.component.ts
@@ -129,22 +129,23 @@ export class EditUserComponent implements OnInit {
       }, { onlySelf: true })
       // }
       // if (th)
-      if (this.selectedUser.role === 'fip' && this.selectedUser.sap === true) {
+      if (this.selectedUser.role === 'fip') {
         this.editUserForm.controls['firstName'].disable({ onlySelf: true });
         this.editUserForm.controls['lastName'].disable({ onlySelf: true });
         this.editUserForm.controls['email'].disable({ onlySelf: true });
         this.editUserForm.controls['username'].disable({ onlySelf: true });
-      } else {
-        this.editUserForm.controls['firstName'].disable({ onlySelf: true });
-        this.editUserForm.controls['lastName'].disable({ onlySelf: true });
-        this.editUserForm.controls['email'].disable({ onlySelf: true });
-        this.editUserForm.controls['username'].disable({ onlySelf: true });
-        this.editUserForm.controls['password'].disable({ onlySelf: true });
-        this.editUserForm.controls['role'].clearValidators();
-        this.editUserForm.controls['role'].disable({ onlySelf: true });
-        this.editUserForm.controls['org'].clearValidators();
-        this.editUserForm.controls['org'].disable({ onlySelf: true });
       }
+      // else {
+      //   this.editUserForm.controls['firstName'].disable({ onlySelf: true });
+      //   this.editUserForm.controls['lastName'].disable({ onlySelf: true });
+      //   this.editUserForm.controls['email'].disable({ onlySelf: true });
+      //   this.editUserForm.controls['username'].disable({ onlySelf: true });
+      //   this.editUserForm.controls['password'].disable({ onlySelf: true });
+      //   this.editUserForm.controls['role'].clearValidators();
+      //   this.editUserForm.controls['role'].disable({ onlySelf: true });
+      //   this.editUserForm.controls['org'].clearValidators();
+      //   this.editUserForm.controls['org'].disable({ onlySelf: true });
+      // }
     });
   }
 
