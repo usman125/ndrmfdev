876d90ac610ddf9b30990c649d90512fb6ccd395
Usman Ali Khan <usmanalikhan00@gmail.com>
2021-04-14 14:00:48 +0500

    changes to the local server API link

--- src/app/services/config.ts
+++ src/app/services/config.ts
@@ -4,7 +4,7 @@ export const AppConfig = {
   // apiUrl: 'http://10.53.14.237:8080'
   // apiUrl: 'https://ndrmfdev-backend.herokuapp.com/api'
   // apiUrl: 'https://attendance.tallymarkscloud.com:8080/ndrmf'
-  // apiUrl: 'https://ndrmf.tallymarkscloud.com:8443/ndrmf'
-  apiUrl: 'http://localhost:8080/ndrmf'
+  apiUrl: 'https://ndrmf.tallymarkscloud.com:8443/ndrmf'
+  // apiUrl: 'http://localhost:8080/ndrmf'
   // apiUrl: 'http://103.226.216.96:8088'
 };
