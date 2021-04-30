b51c8e8b2a30e7992db03dc7a1658df839886565
Usman Ali Khan <usmanalikhan00@gmail.com>
2021-04-30 20:09:40 +0500

    adding safi code and some features

--- src/app/grievance-registration/grievance-registration.component.ts
+++ src/app/grievance-registration/grievance-registration.component.ts
@@ -2,9 +2,9 @@ import { LoginService } from './../services/login.service';
 import { Input, ViewChild } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
-import { FormGroup, FormBuilder, Validators} from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from "@angular/router";
-import * as moment from "moment"; 
+import * as moment from "moment";
 import { SignaturePad } from 'angular2-signaturepad';
 import { ConfirmModelService } from 'src/app/services/confirm-model.service';
 @Component({
@@ -18,9 +18,9 @@ export class GrievanceRegistrationComponent implements OnInit {
   @Input() text = 'Upload';
   /** Name used in form which will be sent in HTTP request. */
   @Input() param = 'file';
-   myDate = new Date().toISOString();
+  myDate = new Date().toISOString();
 
-   formatteddate = moment(this.myDate).format("DD-MM-YYYY  HH:mm");
+  formatteddate = moment(this.myDate).format("DD-MM-YYYY  HH:mm");
   public signaturePadOptions: Object = { // passed through to szimek/signature_pad constructor
     'minWidth': 2,
     'canvasWidth': 500,
@@ -29,8 +29,8 @@ export class GrievanceRegistrationComponent implements OnInit {
   };
 
   public registrationForm: FormGroup;
-   testUser: any;
-   files: any = [];
+  testUser: any;
+  files: any = [];
   fd: FormData;
   responseReult: Object;
   constructor(private formBuilder: FormBuilder,
@@ -40,7 +40,7 @@ export class GrievanceRegistrationComponent implements OnInit {
     private userServices: UserService) {
     this.grievanceFormBuilder();
   }
-ngAfterViewInit() {
+  ngAfterViewInit() {
     // this.signaturePad is now available
     this.signaturePad.set('minWidth', 5); // set szimek/signature_pad options at runtime
     this.signaturePad.clear(); // invoke functions from szimek/signature_pad API
@@ -69,15 +69,15 @@ ngAfterViewInit() {
       // this.uploadFiles();
       console.log("Uploaded Files:---", this.files);
 
-         this.fd = new FormData();
-     this.fd.append(this.param, this.files[0].data);
-  
-    
+      this.fd = new FormData();
+      this.fd.append(this.param, this.files[0].data);
+
+
 
 
     };
     fileUpload.click();
-  
+
   }
 
   goBack() {
@@ -86,85 +86,85 @@ ngAfterViewInit() {
   grievanceFormBuilder() {
     this.registrationForm = this.formBuilder.group({
       'address': ['', [Validators.required]],
-      'cnic':['', [Validators.required]],
-      'complainantName':['', [Validators.required]],
-      'complainantSignature':[''],
-      'complaintAgainstPersonOrDepartment':[''],
-      'complaintCategory':['', [Validators.required]],
-      'complaintDateTime':[''],
-      'complaintLocation':[''],
-      'complaintStatus':[''],
-      'complaintSubLocation':[''],
-      'consultantName':['', [Validators.required]],
-      'contactNo':[''],
-      'email':['', [Validators.email]],
-      'gender':[''],
-      'organizationName':[''],
-      'shortDescFectorCausingProblem':[''],
-      'shortDescription':[''],
-      'vendor':['']
+      'cnic': ['', [Validators.required]],
+      'complainantName': ['', [Validators.required]],
+      'complainantSignature': [''],
+      'complaintAgainstPersonOrDepartment': [''],
+      'complaintCategory': ['', [Validators.required]],
+      'complaintDateTime': [''],
+      'complaintLocation': [''],
+      'complaintStatus': [''],
+      'complaintSubLocation': [''],
+      'consultantName': ['', [Validators.required]],
+      'contactNo': [''],
+      'email': ['', [Validators.email]],
+      'gender': [''],
+      'organizationName': [''],
+      'shortDescFectorCausingProblem': [''],
+      'shortDescription': [''],
+      'vendor': ['']
     })
   }
   get fc() {
     return this.registrationForm.controls;
   }
 
-registerGrievanceUser(){
-  if(!this.fc.cnic.value){
-    console.log("cnic value", this.fc.cnic.value )
-    return;
-  }
-  let complaintToSendData = {
-    "address": this.fc.address.value,
-    "cnic": this.fc.cnic.value,
-    "complainantName": this.fc.complainantName.value,
-    "complainantSignature": this.fc.complainantSignature.value,
-    "complaintAgainstPersonOrDepartment": this.fc.complaintAgainstPersonOrDepartment.value,
-    "complaintCategory": this.fc.complaintCategory.value,
-    "complaintDateTime": this.myDate,
-    "complaintLocation": this.fc.complaintLocation.value,
-    "complaintSubLocation":this.fc.complaintSubLocation.value,
-    "consultantName": this.fc.consultantName.value,
-    "email": this.fc.email.value,
-    "organizationName": this.fc.organizationName.value,
-    "shortDescFectorCausingProblem": this.fc.shortDescFectorCausingProblem.value,
-    "shortDescription":this.fc.shortDescription.value,
-    "vendor": this.fc.vendor.value,
-
-
-  }
-  console.log("registerd Complaint", complaintToSendData)
-
- 
-  this.loginservices.reggisterComplaint(complaintToSendData).subscribe(
-    res => {
-      if(this.fd){
-        this.userServices.fileUpload(res['complaintId'], this.fd).subscribe(
-          res => {
-            console.log("fileupload api is hitting", res)
-          }
-        )
-      }
-     
-    
-console.log("hitting successfully",res)
-const options = {
-  title: 'Successfully  added!',
-  message: 'OK to exit',
-  cancelText: 'CANCEL',
-  confirmText: 'OK',
-  add: true,
-  confirm: false,
-};
-
-this._confirmModelService.open(options);
-this._router.navigate(['login']);
-
-    },
-    _err => {
-      var error = _err.json();
-   console.log(error)
+  registerGrievanceUser() {
+    if (!this.fc.cnic.value) {
+      console.log("cnic value", this.fc.cnic.value)
+      return;
     }
-  )
-}
+    let complaintToSendData = {
+      "address": this.fc.address.value,
+      "cnic": this.fc.cnic.value,
+      "complainantName": this.fc.complainantName.value,
+      "complainantSignature": this.fc.complainantSignature.value,
+      "complaintAgainstPersonOrDepartment": this.fc.complaintAgainstPersonOrDepartment.value,
+      "complaintCategory": this.fc.complaintCategory.value,
+      "complaintDateTime": this.myDate,
+      "complaintLocation": this.fc.complaintLocation.value,
+      "complaintSubLocation": this.fc.complaintSubLocation.value,
+      "consultantName": this.fc.consultantName.value,
+      "email": this.fc.email.value,
+      "organizationName": this.fc.organizationName.value,
+      "shortDescFectorCausingProblem": this.fc.shortDescFectorCausingProblem.value,
+      "shortDescription": this.fc.shortDescription.value,
+      "vendor": this.fc.vendor.value,
+
+
+    }
+    console.log("registerd Complaint", complaintToSendData)
+
+
+    this.loginservices.reggisterComplaint(complaintToSendData).subscribe(
+      res => {
+        if (this.fd) {
+          this.userServices.fileUpload(res['complaintId'], this.fd).subscribe(
+            res => {
+              console.log("fileupload api is hitting", res)
+            }
+          )
+        }
+
+
+        console.log("hitting successfully", res)
+        const options = {
+          title: 'Successfully  added!',
+          message: 'OK to exit',
+          cancelText: 'CANCEL',
+          confirmText: 'OK',
+          add: true,
+          confirm: false,
+        };
+
+        this._confirmModelService.open(options);
+        this._router.navigate(['login']);
+
+      },
+      _err => {
+        var error = _err.json();
+        console.log(error)
+      }
+    )
+  }
 }
